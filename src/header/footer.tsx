a20850bef680a4a80df86a3fcb7037b611af6049
unknown <1533540012@qq.com>
2023-05-16 21:48:06 +0800

    日常提交

---
+++ src/header/footer.tsx
@@ -0,0 +1,8 @@
+import { h } from 'fre'
+
+export default function Footer(props) {
+    return <footer>
+        <p>© 2022 - 2023 tmread.cc & tmread Team <a href="https://github.com/fre-tmread" class='github'>&nbsp;Github.&nbsp;&nbsp; </a> CDN 由<a href="https://netdun.net
+"><img src="https://cdn-us.imgs.moe/2023/02/05/63dfca2a315d8.png" alt="" /></a>赞助</p>
+    </footer>
+}
---
+++ src/home/home.tsx
@@ -0,0 +1,55 @@
+import { h, useEffect, useState } from 'fre'
+import { getPost, getPostDetail, getRank } from '../util/api'
+import { ListA } from '../list/list'
+import { ListB } from '../list/list'
+import './home.css'
+import { buildVideos, Eplayer } from '../play/play'
+import { getSuo } from '../util/avatar'
+
+export default function Home() {
+    const [recommend, setRecommend] = useState([])
+    const [index, setIndex] = useState(0)
+    const [play, setPlay] = useState("")
+    const [ugc, setUgc] = useState([])
+    const [rank, setRank] = useState([])
+    useEffect(() => {
+        getPost('', '独播', 1, 8).then((res: any) => {
+            setRecommend(res.posts)
+        })
+        getRank().then((res2: any) => {
+            setRank(res2.posts.splice(0, 8))
+        })
+        getPost('', '原创', 1, 12).then((res: any) => {
+            setUgc(res.posts)
+        })
+    }, [])
+
+    useEffect(() => {
+        recommend.length > 0 && getPostDetail(recommend[index].id).then(res => {
+            const videos = res.result.videos
+            const play = buildVideos(videos)[0][1]
+            setPlay(play)
+        })
+    }, [index, recommend])
+
+    return (
+        <div>
+            <div class="wrap home">
+                <div className="ep-wrap2">
+                    <Eplayer url={play}></Eplayer>
+                </div>
+                <ul class="imglist">
+                    {recommend.map((item, i) => {
+                        return <li class={i === index ? 'active' : ''} onclick={() => setIndex(i)}><img src={getSuo(item.content)} /></li>
+                    })}
+                </ul>
+
+                <h1>原创，努力做！</h1>
+                <ListA posts={ugc} />
+
+                <h1>番剧，虽然不多，但还有...</h1>
+                <ListB posts={rank} />
+            </div>
+        </div>
+    )
+}
